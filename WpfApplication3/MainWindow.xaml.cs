56b94a3f822699fda8bb7a7033975bc8d4e01235
Hedrick <djkacpl@gmail.com>
2018-09-28 18:58:24 +0200

    Generowanie na podstawie danych ale robiąc swoje liczby

--- WpfApplication3/MainWindow.xaml.cs
+++ WpfApplication3/MainWindow.xaml.cs
@@ -86,6 +86,59 @@ namespace WpfApplication3
 
         }
 
+        public class Spline3Deg { //klasa funkcji sklejanej 3. stopnia, służącą za ciągłą funkcję prawdopodobieństwa
+            private int n;
+            private double[] a, b, c, d, x;
+
+            public Spline3Deg(double[] x, double[] y) {
+                n = x.Length - 1;
+                Array.Copy(x, this.x = new double[n+1], n+1);
+                Array.Copy(y, this.a = new double[n+1], n+1);
+                b = new double[n];
+                c = new double[n+1];
+                d = new double[n];
+
+                double[] h = new double[n];
+                for (int i = 0; i < n; i++)
+                    h[i] = x[i + 1] - x[i];
+                double[] alpha = new double[n - 1];
+                for (int i = 1; i < n; i++)
+                    alpha[i-1] = (3 * (a[i + 1] - a[i]) / h[i] - 3 * (a[i] - a[i - 1]) / h[i - 1]);
+
+                double[] l = new double[n+1], 
+                         mu = new double[n+1], 
+                         z = new double[n+1];
+                l[0] = 1;
+                mu[0] = 0;
+                z[0] = 0;
+
+                for (int i = 1; i < n; ++i)
+                {
+                    l[i] = 2 * (x[i + 1] - x[i - 1]) - h[i - 1] * mu[i - 1];
+                    mu[i] = h[i] / l[i];
+                    z[i] = (alpha[i - 1] - h[i - 1] * z[i - 1]) / l[i];
+                }
+
+                l[n] = 1;
+                z[n] = 0;
+                c[n] = 0;
+
+                for (int j = n - 1; j >= 0; --j)
+                {
+                    c[j] = z[j] - mu[j] * c[j + 1];
+                    b[j] = (a[j + 1] - a[j]) / h[j] - h[j] * (c[j + 1] + 2 * c[j]) / 3;
+                    d[j] = (c[j + 1] - c[j]) / 3 / h[j];
+                }
+            }
+            public double y(double x0) {
+                for (int i = 0; i < n; i++)
+                    if (x0 >= x[i] && x0 <= x[i+1])
+                        return Math.Pow(x0 - x[i], 3) * d[i] + Math.Pow(x0 - x[i], 2) * c[i] + (x0 - x[i]) * b[i] + a[i];
+                return Double.NaN;
+            }
+            public Tuple<double, double> Limits() { return new Tuple<double, double>(x[0], x[n]); }
+        }
+
         public List<KeyValuePair<string, int>> classes { get; set; }
         public List<KeyValuePair<string, Attribute>> attrs { get; set; }
         public List<List<int>> classAttrs { get; set; }
@@ -297,11 +350,14 @@ namespace WpfApplication3
             int classes = 0, attribs = 0;
             //podstawowy słownik - klasy //tuple - zliczanie wystąpień   //drugi słownik - atrybuty
             //trzeci słownik - unikatowe wartości i zliczanie ich wystąpień      //zjebany kod
+            List<string[]> reading = new List<string[]>(), 
+                generating = new List<string[]>(); // do ewentualnego sprawdzania
             using (var read = new System.IO.StreamReader(path)) {
                 string s;
                 while (!string.IsNullOrEmpty(s = read.ReadLine()))
                 {
                     string[] res = s.Split(',');
+                    reading.Add(res);
                     if (!res[0].Equals("Class")) { //jeśli jest linia opisująca, pomiń (z tego generatora jest)
                         if (!values.ContainsKey(res[0]))
                             values.Add(res[0], new Tuple<int, Dictionary<int, Dictionary<string, int>>>(0,
@@ -336,24 +392,78 @@ namespace WpfApplication3
             classes = values.Count();
             attribs = values.ElementAt(0).Value.Item2.Count();
 
-            //czas generować ten szajs
-            var newStuff = new string[classes * 30, attribs + 1]; ;
-            for (int it = 0; it<classes*30;it++) {
-                int cl = rnd.Next(classes); //rnd to zadelkarowany wcześniej Random //losowanie klasy
-                newStuff[it,0] = values.ElementAt(cl).Key;
-                for (int v = 1; v <= attribs; v++) {
-                    int val = rnd.Next(values.ElementAt(cl).Value.Item1);  //losowanie wartości atrybutu
-                    int b = 0;
-                    foreach (var a in values.ElementAt(cl).Value.Item2[v]) {
-                        if(val < (b+= a.Value)){
-                            newStuff[it, v ] = a.Key; //na Monte Carlo
+            //tutaj dorabiam myk na sprawdzanie typu każdego z atrybutów
+            string[] attrType = new string[attribs];
+            int tempI; double tempD;
+            for (int i = 0; i < attribs; i++) {
+                foreach (var v in values) {
+                    foreach (var w in v.Value.Item2.ElementAt(i).Value) {
+                        if (int.TryParse(w.Key, out tempI))
+                            attrType[i] = "integer";
+                        else if (double.TryParse(w.Key, 
+                            System.Globalization.NumberStyles.AllowDecimalPoint, 
+                            System.Globalization.NumberFormatInfo.InvariantInfo,
+                            out tempD))
+                            attrType[i] = "double";
+                        else {
+                            attrType[i] = "string";
                             break;
                         }
+                    }
+                    if (attrType[i].Equals("string")) break;
+                }
+            }
+
+            //tutaj dorzucam tworzenie wykresu ciągłego prawdopodobieństwa
+            Spline3Deg[,] probabilities = new Spline3Deg[classes, attribs];
+            for (int i = 0; i < attribs; i++)
+                if (attrType[i].Equals("double") || attrType[i].Equals("integer"))
+                    for (int j = 0; j < classes; j++) {
+                        int c = values.ElementAt(j).Value.Item2.ElementAt(i).Value.Count;
+                        double[] y, x = new double[c];
+                        SortedList<double, int> temp = new SortedList<double, int>();
+                        foreach (var v in values.ElementAt(j).Value.Item2.ElementAt(i).Value){
+                            int tI = v.Value; double tD = Double.Parse(v.Key, 
+                                System.Globalization.NumberStyles.AllowDecimalPoint, 
+                                System.Globalization.NumberFormatInfo.InvariantInfo);
+                            temp.Add(tD, tI);
+                        }
+                        y = temp.Keys.ToArray();
+                        x[0] = 0;
+                        for (int k = 1; k < temp.Count; k++)
+                            x[k] = x[k - 1] + temp.ElementAt(k - 1).Value + temp.ElementAt(k).Value;
+                        probabilities[j,i] = new Spline3Deg(x,y);
+                    }
+
+            //czas generować ten szajs
+            var newStuff = new string[classes * reading.Count * 20, attribs + 1]; 
+            for (int it = 0; it<newStuff.GetLength(0); it++) {
+                int cl = rnd.Next(classes); //rnd to zadelkarowany wcześniej Random //losowanie klasy
+                newStuff[it,0] = values.ElementAt(cl).Key;
+                for (int v = 1; v <= attribs; v++) {  //losowanie wartości atrybutu
+                    if (attrType[v - 1].Equals("string")){  //funkcja dyskretna
+                        int val = rnd.Next(values.ElementAt(cl).Value.Item1);  
+                        int b = 0;
+                        foreach (var a in values.ElementAt(cl).Value.Item2[v]){
+                            if (val < (b += a.Value)){
+                                newStuff[it, v] = a.Key; //na Monte Carlo
+                                break;
+                            }
+                        }
+                    }else{  //funkcja ciągła
+                        Tuple<double, double> extr = probabilities[cl, v - 1].Limits();
+                        double val = rnd.Next((int)extr.Item1, (int)extr.Item2) + rnd.NextDouble();
+                        double r = probabilities[cl, v - 1].y(val);
+                        if (attrType[v - 1].Equals("double"))
+                            newStuff[it, v] = r.ToString("0.0000", System.Globalization.CultureInfo.InvariantCulture);
+                        else //if (attrType[v - 1].Equals("integer"))
+                            newStuff[it, v] = Math.Round(r).ToString();
                     }//koniec losowania wartości atrybutu
                 }//koniec generowania obiektu
             }//koniec całego generowania
 
             //tu dać zapis do pliku
+            string savefiledir = "";
             using (var dirB = new System.Windows.Forms.SaveFileDialog())
             {
                 dirB.Filter = "Text Files | *.txt";
@@ -361,26 +471,115 @@ namespace WpfApplication3
                 var res = dirB.ShowDialog();
                 if (res == System.Windows.Forms.DialogResult.OK)
                 {
-                    using (var write = new System.IO.StreamWriter(dirB.FileName))
+                    using (var write = new System.IO.StreamWriter(savefiledir = dirB.FileName))
                     {
                         for (int x = 0; x < newStuff.GetLength(0); x++){
                             string line = "";
                             for (int y = 0; y < newStuff.GetLength(1); y++){
                                 line += newStuff[x, y] + ',';
                             }
-                            write.WriteLine(line.Remove(line.Length -1));
+                            line = line.Remove(line.Length - 1);
+                            generating.Add(line.Split(','));
+                            write.WriteLine(line);
                         }
                     }
                 }
             }
             //tu dać walidację wygenerowanych danych
 
-            var dialogResult = System.Windows.MessageBox.Show("Do you want to test the generated data?", "Data testing - crossvalidation", System.Windows.MessageBoxButton.YesNo);
+            var dialogResult = System.Windows.MessageBox.Show("Do you want to test the generated data?", "Data testing - extended data", System.Windows.MessageBoxButton.YesNo);
             if (dialogResult == MessageBoxResult.Yes)
             {
+
+                //podzielić dane wejściowe i wygenerowane na klasy i artybuty
+                var readClass = new int[reading.Count];
+                var readAttr = new int[reading.Count, reading.ElementAt(0).Length-1].ToJagged();
+
+                var stringIntCheatSheet = new Dictionary<string, int>[reading.ElementAt(0).Length];
+                for(int i = 0; i < stringIntCheatSheet.Length;i++)
+                    stringIntCheatSheet[i] = new Dictionary<string, int>();
+
+                for (int x = 0; x < reading.Count; x++) {
+                    for (int y = 0; y < reading.ElementAt(0).Length; y++) {
+                        int res = 0;
+                        string ss = reading.ElementAt(x)[y];
+                        if (!int.TryParse(ss, out res) || y == 0) {
+                            if (!stringIntCheatSheet[y].ContainsKey(ss))
+                                stringIntCheatSheet[y].Add(ss, stringIntCheatSheet[y].Count);
+                            res = stringIntCheatSheet[y][ss];
+                        }
+                        if (y == 0) readClass[x] = res;
+                        else readAttr[x][y - 1] = res;
+                    }
+                }
+
+                var genClass = new int[generating.Count];
+                var genAttr = new int[generating.Count, generating.ElementAt(0).Length - 1].ToJagged();
+
+                for (int x = 0; x < generating.Count; x++)
+                {
+                    for (int y = 0; y < generating.ElementAt(0).Length; y++)
+                    {
+                        int res = 0;
+                        string ss = generating.ElementAt(x)[y];
+                        if (!int.TryParse(ss, out res) || y == 0)
+                        {
+                            if (!stringIntCheatSheet[y].ContainsKey(ss))
+                                stringIntCheatSheet[y].Add(ss, stringIntCheatSheet[y].Count);
+                            res = stringIntCheatSheet[y][ss];
+                        }
+                        if (y == 0) genClass[x] = res;
+                        else genAttr[x][y - 1] = res;
+                    }
+                }
+
+               
+
+
+                int correct=0, incorrect=0, correctknn = 0, incorrectknn = 0;
+
+                double[][] readAttr_d = readAttr.Select(xa => xa.Select(ya => (double)ya).ToArray()).ToArray(),
+                        genAttr_d = genAttr.Select(xa => xa.Select(ya => (double)ya).ToArray()).ToArray();
+
+                
+                var learn = new NaiveBayesLearning();
+                NaiveBayes nb = learn.Learn(readAttr, readClass);
+                var test = nb.Decide(genAttr);
+                foreach (var v in test)
+                {
+                    if (v.Equals(genClass[test.IndexOf(v)]))
+                        correct++;
+                    else
+                        incorrect++;
+                }
+
+                var learnKnn = new KNearestNeighbors(4);
+                    
+                var knn = learnKnn.Learn(readAttr_d, readClass);
+
+                var testknn = knn.Decide(genAttr_d);
+                foreach (var v in testknn)
+                {
+                    if (v.Equals(genClass[testknn.IndexOf(v)]))
+                        correctknn++;
+                    else
+                        incorrectknn++;
+                }
+                
+                System.Windows.MessageBox.Show("Naive Bayes Classification:\nGenerated data accuracy: " + 
+                    100.0 * correct / (correct+incorrect) + "%\n"+
+                   "K Nearest Neighbours Classification:\nGenerated data accuracy: " + 
+                   100.0 * correctknn / (correctknn + incorrectknn)
+                   + "%\n", "Data Testing - extending dataset",
+                    System.Windows.MessageBoxButton.OK);
+
+                
+                /*
                 using (var read = new System.IO.StreamReader(path))
                 {
-                    List<string[]> reading = new List<string[]>();
+
+
+                    //List<string[]> reading = new List<string[]>();
                     Dictionary<string, int> counting = new Dictionary<string, int>(), 
                         grpToInt = new Dictionary<string, int>();
                     string s;
@@ -429,84 +628,14 @@ namespace WpfApplication3
                             else geninputs[x][y - 1] = int.Parse(newStuff[x,y]);} }
                     double[][] geninputs_d = geninputs.Select(xa => xa.Select(ya => (double)ya).ToArray()).ToArray();
 
+            
+                */
 
-                    var learn = new NaiveBayesLearning();
-                    NaiveBayes nb = learn.Learn(trainInputs, trainOutputs);
-                    /*
-                    var cv = CrossValidation.Create(
-                        k: 10,
-                        learner: (p) => new NaiveBayesLearning(),
-                        loss: (actual, expected, p) => new ZeroOneLoss(expected).Loss(actual),
-                        fit: (teacher, x, y, w) => teacher.Learn(x, y, w),
-                        x: trainInputs, y: trainOutputs
-                    );
-                    
-                    var result = cv.Learn(inputs, outputs);
-                    */
-
-                    int original_correct = 0, original_incorrect = 0, new_correct = 0, new_incorrect = 0;
-                    
-                   // var test = 
-
-                    foreach (var v in outputs) {
-                        var r = nb.Decide(inputs[outputs.IndexOf(v)]);
-                        if (v.Equals(r))
-                            original_correct++;
-                        else
-                            original_incorrect++;
-                    }
-                    var r2 = nb.Decide(geninputs);
-                    foreach (var v in genoutputs)
-                    {
-                        if (v.Equals(r2[genoutputs.IndexOf(v)]))
-                            new_correct++;
-                        else
-                            new_incorrect++;
-                    }
-
-                    System.Windows.MessageBox.Show("Naive Bayes Classification:\n Original data: "+ original_correct + " correct, "
-                       + original_incorrect+" wrong\nGenerated data: " + new_correct + " correct, " + new_incorrect + " wrong", 
-                        "Data Testing - extending dataset" , 
-                        System.Windows.MessageBoxButton.OK);
-                    /*
-                    int numberOfSamples = result.NumberOfSamples;
-                    int numberOfInputs = result.NumberOfInputs;
-                    int numberOfOutputs = result.NumberOfOutputs;
-
-                    double trainingError = result.Training.Mean;
-                    double validationError = result.Validation.Mean;
-                    GeneralConfusionMatrix gcm = result.ToConfusionMatrix(inputs, outputs);
-                    double nb_accuracy = gcm.Accuracy;
-                    
-                    //..................    
-
-
-                    var crossvalidation = CrossValidation.Create(
-                        k: 3,
-                        learner: (p) => new KNearestNeighbors(k: 4),
-                        loss: (actual, expected, p) => new ZeroOneLoss(expected).Loss(actual),
-                        fit: (teacher, x, y, w) => teacher.Learn(x, y, w),
-                        x: inputs_d, y: outputs
-                        );
-                    var result2 = crossvalidation.Learn(inputs_d, outputs);
-                    // We can grab some information about the problem:
-                    numberOfSamples = result.NumberOfSamples;
-                    numberOfInputs = result.NumberOfInputs;
-                    numberOfOutputs = result.NumberOfOutputs;
-
-                    trainingError = result2.Training.Mean;
-                    validationError = result2.Validation.Mean;
-
-                    // If desired, compute an aggregate confusion matrix for the validation sets:
-                    gcm = result2.ToConfusionMatrix(inputs_d, outputs);
-                    double knn_accuracy = gcm.Accuracy;
-
-                    System.Windows.MessageBox.Show("Naive Bayes Accuracy: " + nb_accuracy +
-                    "\nk Nearest Neighbors Accuracy: " + knn_accuracy, "Data testing - crossvalidation", System.Windows.MessageBoxButton.OK);
-                    */
-                }
-
-
+            }
+            dialogResult = System.Windows.MessageBox.Show("Do you want to open the file with generated data?", "Data testing - extended data", System.Windows.MessageBoxButton.YesNo);
+            if (dialogResult == MessageBoxResult.Yes)
+            {
+                System.Diagnostics.Process.Start(savefiledir);
             }
         }
 
